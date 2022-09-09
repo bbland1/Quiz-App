4f7933305d8fdcc844d7287b423f04230f9d7cb4
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-09-08 23:28:42 -0400

    removed resource css for an idea no longer doing

--- src/Components/Resources.jsx
+++ src/Components/Resources.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import NavBar from "./NavBar";
-import './recources.css';
 
 function Resources() {
   return (
@@ -11,32 +10,6 @@ function Resources() {
 
       <h3>Colleen's Manga Recs Video</h3>
       <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/169rKJBBc98" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      {/* <div class="timeline">
-        <div class="container left">
-          <div class="content">
-            <h2>2017</h2>
-            <p>Lorem ipsum..</p>
-          </div>
-        </div>
-        <div class="container right">
-          <div class="content">
-            <h2>2016</h2>
-            <p>Lorem ipsum..</p>
-          </div>
-        </div>
-        <div class="container left">
-          <div class="content">
-            <h2>2017</h2>
-            <p>Lorem ipsum..</p>
-          </div>
-        </div>
-        <div class="container right">
-          <div class="content">
-            <h2>2016</h2>
-            <p>Lorem ipsum..</p>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
