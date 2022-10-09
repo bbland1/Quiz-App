0083fabf11cf0a4cfbe3813da7ce3646e046d951
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-10-09 00:41:57 -0400

    pushing changes made before messing wiht my computer

--- src/Components/QuizItems/QuizQuestions.js
+++ src/Components/QuizItems/QuizQuestions.js
@@ -5,7 +5,7 @@ const questions = [
     answersOptions: [
       { id: "A", answerText: "Any romance series set in high school", isCorrect: false },
       { id: "B", answerText: "Stories with female main characters", isCorrect: false },
-      { id: "C", answerText: "Series marketed to young girls and  published in a Shoujo Magazine", isCorrect: true },
+      { id: "C", answerText: "Series marketed to young girls and published in a Shoujo Magazine", isCorrect: true },
       { id: "D", answerText: "Any series a girl would like", isCorrect: false }
     ]
   },
@@ -21,7 +21,7 @@ const questions = [
   },
   {
     question_img: "images/horimiya.jpg",
-    question_description: "Synopsis: On the surface, the thought of Kyouko Hori and Izumi Miyamura getting along would be the last thing in people's minds. After all, Hori has a perfect combination of beauty and brains, while Miyamura appears meek and distant to his fellow classmates. However, a fateful meeting between the two lays both of their hidden selves bare. Even though she is popular at school, Hori has little time to socialize with her friends due to housework. On the other hand, Miyamura lives under the noses of his peers, his body bearing secret tattoos and piercings that make him look like a gentle delinquent. Having opposite personalities yet sharing odd similarities, the two quickly become friends and often spend time together in Hori's home. As they both emerge from their shells, they share with each other a side of themselves concealed from the outside world.",
+    question_description: "Synopsis: A secret life is the one thing they have in common. At school, Hori is a prim and perfect social butterfly, but the truth is she's a brash homebody. Meanwhile, under a gloomy facade, Miyamura hides a gentle heart, along with piercings and tattoos. In a chance meeting, they both reveal a side they've never shown. Could this blossom into something new? (Source: Funimation)",
     questionText: "Is Horimiya a Shoujo?",
     answersOptions: [
       { id: "A", answerText: "It's a shoujo because it is about romance.", isCorrect: false },
--- src/Components/ResourcesPage.jsx
+++ src/Components/ResourcesPage.jsx
@@ -4,15 +4,16 @@ import NavBar from "./NavBar";
 function Resources() {
   return (
     <div>
-      <NavBar />
-      <h1>Resources</h1>
-      <h2>Where the idea started and the info was got</h2>
+      <header className="header-title">
+        <NavBar />
+        <h1>Resources</h1>
+      </header>
       <main className="main-part">
+        <h2>Where the idea started and the info was got</h2>
         <div className="videos-resources">
           <div className="videos">
             <h3>Colleen's Manga Recs Video</h3>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/169rKJBBc98" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            <p>This video was perfect to me, it gave a good laugh and the first "Is this a Shoujo?" gave me a good laugh.</p>
           </div>
           <div className="videos">
             <h3>The Japan Foundation, New York Video</h3>
