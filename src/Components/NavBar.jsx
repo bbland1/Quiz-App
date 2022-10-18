353e074637ee5fa4dca242929ba66e0ed7775e3e
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-10-17 20:09:38 -0400

    update navbar active style

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavBar(){
+function NavBar() {
+  let activeNav = "active-nav";
+
   return (
     <nav>
       <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/quiz">Quiz</NavLink></li>
-        <li><NavLink to="/links">Links</NavLink></li>
+        <li><NavLink to="/" className={({ isActive }) =>
+          isActive ? activeNav : undefined
+        }>Home</NavLink></li>
+        <li><NavLink to="/quiz" className={({ isActive }) =>
+          isActive ? activeNav : undefined
+        }>Quiz</NavLink></li>
+        <li><NavLink to="/links" className={({ isActive }) =>
+          isActive ? activeNav : undefined
+        }>Links</NavLink></li>
       </ul>
     </nav>
   );
